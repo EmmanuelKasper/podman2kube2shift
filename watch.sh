8b60a48e91c60efb500d674581160f167c2457c1
Emmanuel Kasper <manu@debian.org>
2021-03-02 16:41:26 +0100

    Improve watch script

--- watch.sh
+++ watch.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 ROUTE=$(oc get -n hello route -o json | jq '.items[0].spec.host'| sed 's/"//g')
-echo $ROUTE
+OUTPUT=/tmp/curl-$(date +%Y-%m-%d)-ovn.log
 
 while true; do
-	date | tee -a /tmp/curl-$(date +%Y-%m-%d).log
-	curl -s http://$ROUTE \
-	| tee -a /tmp/curl-$(date +%Y-%m-%d).log
+	date | tee -a $OUTPUT
+	curl -s http://$ROUTE | tee -a $OUTPUT
 	sleep 2
 done
